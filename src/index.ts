04a1b8074926779c8f8b5ea66f40aea953523d5c
Antonio Aradaza <antonioaradaza9917@gmail.com>
2024-03-13 10:10:25 +0800

    ♻️ refactor: remove unnecessary condition for HOST

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ const PORT = Number(process.env.port) || 8000;
 const HOST = process.env.host || 'localhost';
 
 app.listen(PORT, HOST, () => {
-  console.log('Listening on %s:%d...', HOST || '*', PORT);
+  console.log('Listening on %s:%d...', HOST, PORT);
 });
